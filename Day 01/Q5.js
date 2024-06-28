2829a79903ebd3e929b3ef36d8d05c8f8da66a3e
Nimeshi Dilshara <120201164+n-nimna@users.noreply.github.com>
2024-06-28 13:15:59 +0530

    Q5.js

---
+++ Day 01/Q5.js
@@ -0,0 +1,14 @@
+//Declare variable with different data types
+let name = "Nimna";
+let age  = 23;
+let isRegistered = true;
+let selectedColor = null;
+let favouriteNumber; //This will be undefined
+
+
+//Check the data type of each variable using typeof
+console.log(typeof name);  //Output: string
+console.log(typeof age);   //Output: number
+console.log(typeof isRegistered);   //Output: boolean
+console.log(typeof selectedColor);  //Output: object(**Important note**)
+console.log(typeof favouriteNumber); //Output: undefined
