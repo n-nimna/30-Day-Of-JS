bf390c5ae185c14b8a63b16b4514cce21ff4fc1a
Nimeshi Dilshara <120201164+n-nimna@users.noreply.github.com>
2024-06-28 13:15:28 +0530

    Q4.js

---
+++ Day 01/Q4.js
@@ -0,0 +1,16 @@
+//Declare a variable with a string value
+let message = "This is a string";
+
+//Declare a variable with a boolean value(true)
+let isRunning = true;
+
+//Declare a variable without assigning a value(undefined)
+let maybeLater;
+
+// Declare a variable and explicitly assign null
+let nothingHere = null;
+
+console.log(message);     //output:This is a string
+console.log(isRunning);   //outout:true
+console.log(maybeLater);  //output:undefined
+console.log(nothingHere); //output:null
